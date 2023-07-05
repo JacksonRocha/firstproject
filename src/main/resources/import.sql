3120cc797a25bdc5b3815156d24df41da84ffed4
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-04 21:34:10 -0300

    6.5 Colocando dados no restaurante 1 com endereço para teste no import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,21 +4,23 @@ insert into cozinha (id, nome) values (3, 'Brasileira') ;
 insert into cozinha (id, nome) values (4, 'Chinesa') ;
 insert into cozinha (id, nome) values (5, 'Europeia') ;
 
-insert into restaurante (id,nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro ) values (1, 'Thai', 10.50, 1, 1, '72017-221', 'Quadra', '34', 'Taguatinga');
-insert into restaurante (id,nome, taxa_frete, cozinha_id) values (2, 'Namaste', 11.60, 2);
-insert into restaurante (id,nome, taxa_frete, cozinha_id) values (3, 'Beer_House', 0, 3);
+insert into estado (id, nome) values (1, 'Minas Gerais') ;
+insert into estado (id, nome) values (2, 'São Paulo') ;
+insert into estado (id, nome) values (3, 'Ceará') ;
+
+insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1) ;
+insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1) ;
+insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2 ) ;
+insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2) ;
+insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3) ;
+
+insert into restaurante (id,nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro ) values (1, 'Tailandesa', 10.50, 1, 1, '72017-221', 'Quadra', '34', 'Taguatinga');
+insert into restaurante (id,nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
+insert into restaurante (id,nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 0, 3);
 insert into restaurante (id,nome, taxa_frete, cozinha_id) values (4, 'Qin_Tian', 13.80, 4);
 insert into restaurante (id,nome, taxa_frete, cozinha_id) values (5, 'Croinssanterie', 0, 5);
 
-insert into estado (id, nome) values (1, 'Distrito Federal') ;
-insert into estado (id, nome) values (2, 'São Paulo') ;
-insert into estado (id, nome) values (3, 'Goias') ;
 
-insert into cidade (id, nome, estado_id) values (1, 'Ceilandia', 1) ;
-insert into cidade (id, nome, estado_id) values (2, 'Estrutural', 1) ;
-insert into cidade (id, nome, estado_id) values (3, 'Aguas Lindas', 3 ) ;
-insert into cidade (id, nome, estado_id) values (4, 'Valparaiso', 3) ;
-insert into cidade (id, nome, estado_id) values (5, 'São Paulo', 2) ;
 
 insert into forma_pagamento (id, descricao) values (1, 'Dinheiro') ;
 insert into forma_pagamento (id, descricao) values (2, 'Debito') ;
