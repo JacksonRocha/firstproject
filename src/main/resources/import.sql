268d969e21cb8515e244df8ae70d1bd21149c2e6
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-06 17:17:58 -0300

    6.11 Fazendo alteração na tabela "restaurante_forma_pagamento"

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,6 +15,8 @@ insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2 ) ;
 insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2) ;
 insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3) ;
 
+
+
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Porco com molho agridoce', 'Deliciosa carne suína ao molho especial', 78.90, 1, 1);
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Camarão tailandês', '16 camarões grandes ao molho picante', 110, 1, 1);
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Salada picante com carne grelhada', 'Salada de folhas com cortes finos de carne bovina grelhada e nosso molho especial de pimenta vermelha', 87.20, 1, 2);
@@ -34,16 +36,15 @@ insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_at
 insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (5, 'Lanchoneto do Tio Sam', 11, 4, utc_timestamp, utc_timestamp);
 insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (6, 'Bar da Maria', 6, 4, utc_timestamp, utc_timestamp);
 
-
 insert into forma_pagamento (id, descricao) values (1, 'Dinheiro') ;
 insert into forma_pagamento (id, descricao) values (2, 'Debito') ;
 insert into forma_pagamento (id, descricao) values (3, 'Credito') ;
 insert into forma_pagamento (id, descricao) values (4, 'Pix') ;
 
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3);
+
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas') ;
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas') ;
 
-insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3),
-                                                                                    (3, 2), (3, 3), (4, 1), (4, 2),
-                                                                                    (5, 1), (5, 2), (6, 3);
+
 
