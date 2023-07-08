d0e892082ee3a3688e0a827b352f55a3aa2e811e
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-08 18:51:17 -0300

    7.6 Removendo o exemplo

--- src/main/java/io/github/jackson/domain/model/Cozinha.java
+++ src/main/java/io/github/jackson/domain/model/Cozinha.java
@@ -21,9 +21,6 @@ public class Cozinha {
     @Column(nullable = false)
     private String nome;
 
-    @Column(name = "observacao")
-    private String descricao;
-
     @JsonIgnore
     @OneToMany(mappedBy = "cozinha")
     private List<Restaurante> restaurantes = new ArrayList<>();
