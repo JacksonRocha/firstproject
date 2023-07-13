ae18c8f41f0a8eab26b3531164cad51b6e447b44
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-12 22:22:15 -0300

    7.10 Usando o "ignore" do mysql primeira opção

--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -1,4 +1,4 @@
-insert into cozinha (id, nome) values (1, 'Tailandesa') ;
-insert into cozinha (id, nome) values (2, 'Indiana') ;
-insert into cozinha (id, nome) values (3, 'Argentina') ;
-insert into cozinha (id, nome) values (4, 'Brasileira') ;
+insert ignore into cozinha (id, nome) values (1, 'Tailandesa') ;
+insert ignore into cozinha (id, nome) values (2, 'Indiana') ;
+insert ignore into cozinha (id, nome) values (3, 'Argentina') ;
+insert ignore into cozinha (id, nome) values (4, 'Brasileira') ;
