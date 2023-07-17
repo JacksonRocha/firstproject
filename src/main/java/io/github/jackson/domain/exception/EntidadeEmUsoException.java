cfb6e9692be850da9b1fdc6c4a3590145ba36456
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-17 17:52:04 -0300

    8.14 Removendo o status code da EntidadeEmUsoException.java

--- src/main/java/io/github/jackson/domain/exception/EntidadeEmUsoException.java
+++ src/main/java/io/github/jackson/domain/exception/EntidadeEmUsoException.java
@@ -3,7 +3,6 @@ package io.github.jackson.domain.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
 public class EntidadeEmUsoException extends RuntimeException{
 
     public EntidadeEmUsoException(String mensagem) {
