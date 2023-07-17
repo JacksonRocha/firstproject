c7dcf8f7506b76912b1eccceaacd462feae0d336
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-17 16:39:53 -0300

    8.11 Criando a exception especifica de CidadeNaoEncontradaException.java

---
+++ src/main/java/io/github/jackson/domain/exception/CidadeNaoEncontradaException.java
@@ -0,0 +1,11 @@
+package io.github.jackson.domain.exception;
+
+public class CidadeNaoEncontradaException extends EntidadeNaoEncontradaException {
+    public CidadeNaoEncontradaException(String mensagem) {
+        super(mensagem);
+    }
+
+    public CidadeNaoEncontradaException(Long cidadeId) {
+        this(String.format("Não existe um cadastro de cidade com codigo %d", cidadeId));
+    }
+}
