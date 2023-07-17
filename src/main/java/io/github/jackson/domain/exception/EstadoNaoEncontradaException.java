60e90f4dd9721eba32e13a9b371658af5b33e137
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-17 16:22:47 -0300

    8.10 Deixando a mensagem de erro dentro da exception especifica tirando da service

--- src/main/java/io/github/jackson/domain/exception/EstadoNaoEncontradaException.java
+++ src/main/java/io/github/jackson/domain/exception/EstadoNaoEncontradaException.java
@@ -4,4 +4,8 @@ public class EstadoNaoEncontradaException extends EntidadeNaoEncontradaException
     public EstadoNaoEncontradaException(String mensagem) {
         super(mensagem);
     }
+
+    public EstadoNaoEncontradaException (Long estadoId) {
+        this(String.format("Não existe um cadastro de estado com codigo %d", estadoId));
+    }
 }
