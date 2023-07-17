987948f151d530e81d05f42cb7145bc1a26a67de
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-17 17:11:36 -0300

    8.12 Na classe Problem.java estou acrescentando os campos que quero que mostre no erro

--- src/main/java/io/github/jackson/api/exceptionhandler/Problem.java
+++ src/main/java/io/github/jackson/api/exceptionhandler/Problem.java
@@ -1,4 +1,15 @@
 package io.github.jackson.api.exceptionhandler;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class Problem {
+
+    private LocalDateTime dataHora;
+    private String mensagem;
+
 }
