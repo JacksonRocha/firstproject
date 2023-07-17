15220843cb31f62ebd8b068a75bd37cc70a43d6a
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-17 17:40:05 -0300

    8.13 Criando o metodo para uso errado de xml no lugar de json

--- src/main/java/io/github/jackson/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/io/github/jackson/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,6 +4,7 @@ import io.github.jackson.domain.exception.EntidadeNaoEncontradaException;
 import io.github.jackson.domain.exception.NegocioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -30,6 +31,16 @@ public class ApiExceptionHandler {
                 .mensagem(e.getMessage()).build();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(problema);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<?> tratarMediaTypeNotSupportException() {
+        Problema problema =Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem("O tipo de mídia não é aceito").build();
+
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
                 .body(problema);
     }
 }
