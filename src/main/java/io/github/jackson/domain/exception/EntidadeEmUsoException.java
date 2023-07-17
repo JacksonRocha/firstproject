d3a1c86fbacbf2c4788582f5ec182ce3e9afcf9f
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-17 17:54:04 -0300

    8.14 Removendo os imports

--- src/main/java/io/github/jackson/domain/exception/EntidadeEmUsoException.java
+++ src/main/java/io/github/jackson/domain/exception/EntidadeEmUsoException.java
@@ -1,8 +1,5 @@
 package io.github.jackson.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class EntidadeEmUsoException extends RuntimeException{
 
     public EntidadeEmUsoException(String mensagem) {
