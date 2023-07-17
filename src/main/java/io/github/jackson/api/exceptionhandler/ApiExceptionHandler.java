5b625d951684c2823d4a9beac0ddbd2e1c1f9059
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-17 17:49:38 -0300

    8.13 Criando a exception para EntidadeEmUsoException

--- src/main/java/io/github/jackson/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/io/github/jackson/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package io.github.jackson.api.exceptionhandler;
 
+import io.github.jackson.domain.exception.EntidadeEmUsoException;
 import io.github.jackson.domain.exception.EntidadeNaoEncontradaException;
 import io.github.jackson.domain.exception.NegocioException;
 import org.springframework.http.HttpStatus;
@@ -41,6 +42,17 @@ public class ApiExceptionHandler {
                 .mensagem("O tipo de mídia não é aceito").build();
 
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body(problema);
+    }
+
+    @ExceptionHandler(EntidadeEmUsoException.class)
+    public ResponseEntity<?> tratarEntidadeEmUsoException(
+            EntidadeEmUsoException e) {
+        Problema problema =Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage()).build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(problema);
     }
 }
