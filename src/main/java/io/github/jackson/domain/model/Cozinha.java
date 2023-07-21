1d7e47b6691a250b8dd61c92fc1c8518fe9d4800
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-21 15:27:35 -0300

    9.7 Acrescentando a interface para determinas os grupos de validação

--- src/main/java/io/github/jackson/domain/model/Cozinha.java
+++ src/main/java/io/github/jackson/domain/model/Cozinha.java
@@ -1,8 +1,10 @@
 package io.github.jackson.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.github.jackson.Groups;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -14,6 +16,7 @@ import java.util.List;
 @Entity
 public class Cozinha {
 
+    @NotNull(groups = Groups.CadastroRestaurante.class)
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
