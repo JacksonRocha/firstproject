6e676aaeaf2ab1c9f4b94b0e92943bd6324e9198
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-21 18:20:25 -0300

    9.9 Adicionando @NotBlank no nome e @NotNull @ConvertGroup com o Group.class @Valid na chave estrangeira Estado

--- src/main/java/io/github/jackson/domain/model/Cidade.java
+++ src/main/java/io/github/jackson/domain/model/Cidade.java
@@ -1,6 +1,12 @@
 package io.github.jackson.domain.model;
 
+import io.github.jackson.Groups;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.ConvertGroup;
+import jakarta.validation.groups.Default;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -9,14 +15,19 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class Cidade {
 
+
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(nullable = false)
     private String nome;
 
+    @Valid
+    @ConvertGroup(from = Default.class, to = Groups.EstadoId.class)
+    @NotNull
     @ManyToOne
     @JoinColumn(nullable = false)
     private Estado estado;
