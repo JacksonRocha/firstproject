a9c2894e9d48705969aa6e5163171e3b7470f305
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-24 18:21:05 -0300

    10.8 Alterando o nome da classe de teste

---
+++ src/test/java/io/github/jackson/CadastroCozinhaTesteApiIT.java
@@ -0,0 +1,12 @@
+package io.github.jackson;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class CadastroCozinhaTesteApiIT {
+
+
+
+}
