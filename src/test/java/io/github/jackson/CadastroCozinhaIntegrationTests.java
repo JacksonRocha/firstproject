92781393f4989a5d99d3f9dfbe9a3c590e84fec6
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-24 16:01:10 -0300

    10.3 Criando o primeiro teste que cadastra uma cozinha

--- src/test/java/io/github/jackson/CadastroCozinhaIntegrationTests.java
+++ src/test/java/io/github/jackson/CadastroCozinhaIntegrationTests.java
@@ -1,8 +1,34 @@
 package io.github.jackson;
 
+import io.github.jackson.domain.model.Cozinha;
+import io.github.jackson.domain.service.CadastroCozinhaService;
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class CadastroCozinhaIntegrationTests {
+
+    @Autowired
+    private CadastroCozinhaService cadastroCozinhaService;
+
+    @Test
+    public void testarCadastroCozinhaComSucesso() {
+        // cenário
+        Cozinha novaCozinha = new Cozinha();
+        novaCozinha.setNome("Chinesa");
+
+        // ação
+        novaCozinha =  cadastroCozinhaService.salvar(novaCozinha);
+
+        // validação
+        assertThat(novaCozinha).isNotNull();
+        assertThat(novaCozinha.getId()).isNotNull();
+    }
+
 
 }
