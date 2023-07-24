a2235bfb5042f47ca4c8c3be2f848eafc4c1f2c0
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-24 18:57:25 -0300

    10.9 Criando o teste deveConter5Cozinhas_QuandoConsultarCozinhas

--- src/test/java/io/github/jackson/CadastroCozinhaTesteApiIT.java
+++ src/test/java/io/github/jackson/CadastroCozinhaTesteApiIT.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CadastroCozinhaTesteApiIT {
@@ -25,5 +27,20 @@ public class CadastroCozinhaTesteApiIT {
                 .get()
             .then()
                 .statusCode(200);
+    }
+
+    @Test
+    public void deveConter5Cozinhas_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+            .then()
+            .body("", hasSize(5))
+            .body("nome", hasItems("Indiana", "Tailandesa"));
     }
 }
