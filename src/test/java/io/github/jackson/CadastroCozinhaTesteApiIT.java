92bf7de8c58d4d473069ad54dc9ce77d4dba4789
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-25 14:01:37 -0300

    10.11 Criando teste deveRetornarStatus201_QuandoCadastrarCozinha

--- src/test/java/io/github/jackson/CadastroCozinhaTesteApiIT.java
+++ src/test/java/io/github/jackson/CadastroCozinhaTesteApiIT.java
@@ -2,9 +2,11 @@ package io.github.jackson;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
@@ -15,13 +17,16 @@ public class CadastroCozinhaTesteApiIT {
 
     @LocalServerPort
     private int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+    }
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
         given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
             .when()
                 .get()
@@ -31,16 +36,24 @@ public class CadastroCozinhaTesteApiIT {
 
     @Test
     public void deveConter5Cozinhas_QuandoConsultarCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
         given()
-            .basePath("/cozinhas")
-            .port(port)
             .accept(ContentType.JSON)
         .when()
             .get()
             .then()
             .body("", hasSize(5))
             .body("nome", hasItems("Indiana", "Tailandesa"));
+    }
+
+    @Test
+    public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+        given()
+            .body("{ \"nome\": \"Chinesa\" }")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .post()
+        .then()
+            .statusCode(HttpStatus.CREATED.value());
     }
 }
