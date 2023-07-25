a9fa238d784b97f5a7b03595f589de909a9f9add
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-25 14:56:15 -0300

    10.14 Usando um banco de dados para os testes

--- src/test/java/io/github/jackson/CadastroCozinhaTesteApiIT.java
+++ src/test/java/io/github/jackson/CadastroCozinhaTesteApiIT.java
@@ -1,8 +1,10 @@
 package io.github.jackson;
 
+import io.github.jackson.domain.model.Cozinha;
+import io.github.jackson.domain.repository.CozinhaRepository;
+import io.github.jackson.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,12 +23,20 @@ public class CadastroCozinhaTesteApiIT {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
     @BeforeEach
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
 
+        databaseCleaner.clearTables();
+        prepararDados();
     }
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
@@ -40,14 +49,14 @@ public class CadastroCozinhaTesteApiIT {
     }
 
     @Test
-    public void deveConter5Cozinhas_QuandoConsultarCozinhas() {
+    public void deveConter1Cozinhas_QuandoConsultarCozinhas() {
         given()
             .accept(ContentType.JSON)
         .when()
             .get()
-            .then()
-            .body("", hasSize(5))
-            .body("nome", hasItems("Indiana", "Tailandesa"));
+        .then()
+            .body("", hasSize(1));
+
     }
 
     @Test
@@ -60,5 +69,27 @@ public class CadastroCozinhaTesteApiIT {
             .post()
         .then()
             .statusCode(HttpStatus.CREATED.value());
+    }
+
+    private void prepararDados() {
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Tailandesa");
+        cozinhaRepository.save(cozinha1);
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha1.setNome("Americana");
+        cozinhaRepository.save(cozinha1);
+
+        Cozinha cozinha3 = new Cozinha();
+        cozinha1.setNome("Indiana");
+        cozinhaRepository.save(cozinha1);
+
+        Cozinha cozinha4 = new Cozinha();
+        cozinha1.setNome("Brasileira");
+        cozinhaRepository.save(cozinha1);
+
+        Cozinha cozinha5 = new Cozinha();
+        cozinha1.setNome("Chinesa");
+        cozinhaRepository.save(cozinha1);
     }
 }
