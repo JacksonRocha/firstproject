b3dcfefcf470f5b7ef7bf853f78777ed63e1826a
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-25 14:08:37 -0300

    10.12 Colocando a flyway.migrate() que reseta o banco de dados para uma versão conhecida para cada teste

--- src/test/java/io/github/jackson/CadastroCozinhaTesteApiIT.java
+++ src/test/java/io/github/jackson/CadastroCozinhaTesteApiIT.java
@@ -2,8 +2,10 @@ package io.github.jackson;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -18,11 +20,16 @@ public class CadastroCozinhaTesteApiIT {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
     @BeforeEach
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+
+        flyway.migrate();
     }
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
