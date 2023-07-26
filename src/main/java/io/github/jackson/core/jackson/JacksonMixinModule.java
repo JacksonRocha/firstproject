422231f02c59d0fb3808da444439b797ca36f42e
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-26 10:46:11 -0300

    11.4 Acrescentando CidadeMixin.java e CozinhaMixin.java

--- src/main/java/io/github/jackson/core/jackson/JacksonMixinModule.java
+++ src/main/java/io/github/jackson/core/jackson/JacksonMixinModule.java
@@ -1,7 +1,11 @@
 package io.github.jackson.core.jackson;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import io.github.jackson.api.model.mixin.CidadeMixin;
+import io.github.jackson.api.model.mixin.CozinhaMixin;
 import io.github.jackson.api.model.mixin.RestauranteMixin;
+import io.github.jackson.domain.model.Cidade;
+import io.github.jackson.domain.model.Cozinha;
 import io.github.jackson.domain.model.Restaurante;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +14,8 @@ public class JacksonMixinModule extends SimpleModule {
 
     public JacksonMixinModule() {
         setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+        setMixInAnnotation(Cidade.class, CidadeMixin.class);
+        setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
     }
 
 }
