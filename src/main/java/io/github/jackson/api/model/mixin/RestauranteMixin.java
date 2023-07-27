b151aa4ba8a7a42a4d84c344f5c338b4ee3c72ed
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-27 20:39:32 -0300

    11.7 Colocando OffsetDateTime no RestauranteMixin.java

--- src/main/java/io/github/jackson/api/model/mixin/RestauranteMixin.java
+++ src/main/java/io/github/jackson/api/model/mixin/RestauranteMixin.java
@@ -8,6 +8,7 @@ import io.github.jackson.domain.model.FormaPagamento;
 import io.github.jackson.domain.model.Produto;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 public abstract class RestauranteMixin {
@@ -18,11 +19,11 @@ public abstract class RestauranteMixin {
     @JsonIgnore
     private Endereco endereco;
 
-    @JsonIgnore
-    private LocalDateTime dataCadastro;
+    //@JsonIgnore
+    private OffsetDateTime dataCadastro;
 
-    @JsonIgnore
-    private LocalDateTime dataAtualizacao;
+    //@JsonIgnore
+    private OffsetDateTime dataAtualizacao;
 
     @JsonIgnore
     private List<FormaPagamento> formasPagamento;
