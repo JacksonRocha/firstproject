1d669dbc07dc3be41c9355dbbb99d81428c510ce
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-28 22:12:34 -0300

    11.9 Criando a CozinhaModel.java que sera o modelo de representação

---
+++ src/main/java/io/github/jackson/api/model/CozinhaModel.java
@@ -0,0 +1,12 @@
+package io.github.jackson.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CozinhaModel {
+
+    private Long id;
+    private String nome;
+}
