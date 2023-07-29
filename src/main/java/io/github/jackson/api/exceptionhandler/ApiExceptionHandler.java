cc875f8cf064959b4e8e69d32c9b4b99b4e6cef8
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-07-28 21:37:31 -0300

    11.8 Arrumando o metodo handleExceptionInternal e createProblemBuilder para receber o OffsetDateTime

--- src/main/java/io/github/jackson/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/io/github/jackson/api/exceptionhandler/ApiExceptionHandler.java
@@ -28,6 +28,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -241,7 +242,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         if (body == null) {
             body = Problem.builder()
-                    .timestamp(LocalDateTime.now())
+                    .timestamp(OffsetDateTime.now())
                     .title(statusCode.toString())
                     .status(statusCode.value())
                     .userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
@@ -250,7 +251,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         } else if (body instanceof String) {
             body = Problem.builder()
-                    .timestamp(LocalDateTime.now())
+                    .timestamp(OffsetDateTime.now())
                     .title((String) body)
                     .status(statusCode.value())
                     .userMessage(MSG_ERRO_GENERICA_USUARIO_FINAL)
@@ -302,7 +303,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
             ProblemType problemType, String detail) {
 
         return Problem.builder()
-                .timestamp(LocalDateTime.now())
+                .timestamp(OffsetDateTime.now())
                 .status(status.value())
                 .type(problemType.getUri())
                 .title(problemType.getTitle())
