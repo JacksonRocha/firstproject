baa40e579c86c1d57145c3a5ed181276e5eae1ef
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-01 19:07:24 -0300

    11.9 Alterando o retorno do metodo buscar

--- src/main/java/io/github/jackson/api/controller/RestauranteController.java
+++ src/main/java/io/github/jackson/api/controller/RestauranteController.java
@@ -48,6 +48,7 @@ public class RestauranteController {
     public Restaurante buscar(@PathVariable Long restauranteId) {
        Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
 
+       RestauranteModel restauranteModel = null;
 
                 return restaurante;
     }
