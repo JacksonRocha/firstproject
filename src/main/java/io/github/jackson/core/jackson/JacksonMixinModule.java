b5915eda3be8d96159c6665176e139a882f26ec8
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-07 22:50:10 -0300

    11.12 Removendo o RestauranteMixin do JacksonMixinModule.java

--- src/main/java/io/github/jackson/core/jackson/JacksonMixinModule.java
+++ src/main/java/io/github/jackson/core/jackson/JacksonMixinModule.java
@@ -3,7 +3,6 @@ package io.github.jackson.core.jackson;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import io.github.jackson.api.model.mixin.CidadeMixin;
 import io.github.jackson.api.model.mixin.CozinhaMixin;
-import io.github.jackson.api.model.mixin.RestauranteMixin;
 import io.github.jackson.domain.model.Cidade;
 import io.github.jackson.domain.model.Cozinha;
 import io.github.jackson.domain.model.Restaurante;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Component;
 public class JacksonMixinModule extends SimpleModule {
 
     public JacksonMixinModule() {
-        setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
         setMixInAnnotation(Cidade.class, CidadeMixin.class);
         setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
     }
