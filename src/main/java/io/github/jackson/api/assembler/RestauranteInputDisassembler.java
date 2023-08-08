5bb6c64c016e43e23ba3e7ae0fc49915f1c5a1d7
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-08 10:40:07 -0300

    11.14 Usando o ModelMapper

--- src/main/java/io/github/jackson/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/io/github/jackson/api/assembler/RestauranteInputDisassembler.java
@@ -1,23 +1,18 @@
 package io.github.jackson.api.assembler;
 
 import io.github.jackson.api.model.mixin.input.RestauranteInput;
-import io.github.jackson.domain.model.Cozinha;
 import io.github.jackson.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestauranteInputDisassembler {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+        return modelMapper.map(restauranteInput, Restaurante.class);
     }
 }
