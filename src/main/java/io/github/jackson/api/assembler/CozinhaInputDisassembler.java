0381f33efc9c905016384ca718efd9767c91a99e
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-08 12:45:30 -0300

    11.20 Acrescentando a anotação @Valid pois estava acusando um erro

--- src/main/java/io/github/jackson/api/assembler/CozinhaInputDisassembler.java
+++ src/main/java/io/github/jackson/api/assembler/CozinhaInputDisassembler.java
@@ -1,10 +1,8 @@
 package io.github.jackson.api.assembler;
 
-import io.github.jackson.api.model.mixin.input.CidadeInput;
-import io.github.jackson.api.model.mixin.input.CozinhaIdInput;
 import io.github.jackson.api.model.mixin.input.CozinhaInput;
-import io.github.jackson.domain.model.Cidade;
 import io.github.jackson.domain.model.Cozinha;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,7 +13,7 @@ public class CozinhaInputDisassembler {
     @Autowired
     private ModelMapper modelMapper;
 
-    public Cozinha toDomainObject(CozinhaIdInput cozinhaIdInput) {
+    public Cozinha toDomainObject(@Valid CozinhaInput cozinhaIdInput) {
         return modelMapper.map(cozinhaIdInput, Cozinha.class);
     }
 
