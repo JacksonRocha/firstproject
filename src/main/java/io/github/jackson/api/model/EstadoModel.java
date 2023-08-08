5d690f98dcd532255820bed246c80be7a3f86d52
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-08 11:50:03 -0300

    11.20 Classe a classe model EstadoModel.java

---
+++ src/main/java/io/github/jackson/api/model/EstadoModel.java
@@ -0,0 +1,12 @@
+package io.github.jackson.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class EstadoModel {
+
+    private Long id;
+    private String nome;
+}
