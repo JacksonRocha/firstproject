0f110bfbed01408c64e89b3ddbef3b7c25bcd81d
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-10 23:30:59 -0300

    12.4 Acrescentando o campo ativo em restaurante como TRUE

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -44,12 +44,12 @@ insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2 ) ;
 insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2) ;
 insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3) ;
 
-insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cidade_id, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero, endereco_bairro ) values (1, 'Tailandesa', 10.50, 1, utc_timestamp, utc_timestamp, 1, '72017-221', 'Chacará 25', 'Quadra', '34', 'Taguatinga');
-insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (2, 'Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp);
-insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (3, 'Tuk Tuk Comida Indiana', 0, 3, utc_timestamp, utc_timestamp);
-insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (4, 'Java Steakhouse', 12, 3, utc_timestamp, utc_timestamp);
-insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (5, 'Lanchoneto do Tio Sam', 11, 4, utc_timestamp, utc_timestamp);
-insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (6, 'Bar da Maria', 6, 4, utc_timestamp, utc_timestamp);
+insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, endereco_cidade_id, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero, endereco_bairro ) values (1, 'Tailandesa', 10.50, 1, utc_timestamp, utc_timestamp, true, 1, '72017-221', 'Chacará 25', 'Quadra', '34', 'Taguatinga');
+insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (2, 'Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp, true);
+insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (3, 'Tuk Tuk Comida Indiana', 0, 3, utc_timestamp, utc_timestamp, true);
+insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (4, 'Java Steakhouse', 12, 3, utc_timestamp, utc_timestamp, true);
+insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (5, 'Lanchoneto do Tio Sam', 11, 4, utc_timestamp, utc_timestamp, true);
+insert into restaurante (id,nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (6, 'Bar da Maria', 6, 4, utc_timestamp, utc_timestamp, true);
 
 insert into forma_pagamento (id, descricao) values (1, 'Dinheiro') ;
 insert into forma_pagamento (id, descricao) values (2, 'Debito') ;
