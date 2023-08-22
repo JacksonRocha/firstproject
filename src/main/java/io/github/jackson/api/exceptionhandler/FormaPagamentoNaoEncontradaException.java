479c32022d7fc3b1793761ab2792f323b6acebd6
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-22 18:17:16 -0300

    12.5 Criando a exception para tratar FormaPagamentoNaoEncontradaException.java

---
+++ src/main/java/io/github/jackson/api/exceptionhandler/FormaPagamentoNaoEncontradaException.java
@@ -0,0 +1,14 @@
+package io.github.jackson.api.exceptionhandler;
+
+import io.github.jackson.domain.exception.EntidadeNaoEncontradaException;
+
+public class FormaPagamentoNaoEncontradaException extends EntidadeNaoEncontradaException {
+
+    public FormaPagamentoNaoEncontradaException(String mensagem) {
+        super(mensagem);
+    }
+
+    public FormaPagamentoNaoEncontradaException(Long formaPagamentoId) {
+        this(String.format("Não existe um cadastro de forma de pagamento com código %d", formaPagamentoId));
+    }
+}
