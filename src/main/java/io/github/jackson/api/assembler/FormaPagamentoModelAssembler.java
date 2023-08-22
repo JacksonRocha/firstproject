30b9f5fa139b68b68a98a6ad5a753a428df3ad66
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-22 18:10:23 -0300

    12.5 Criando a classe FormaPagamentoModelAssembler.java

---
+++ src/main/java/io/github/jackson/api/assembler/FormaPagamentoModelAssembler.java
@@ -0,0 +1,28 @@
+package io.github.jackson.api.assembler;
+
+import io.github.jackson.api.model.CozinhaModel;
+import io.github.jackson.domain.model.Cozinha;
+import io.github.jackson.domain.model.FormaPagamento;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class FormaPagamentoModelAssembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public FormaPagamentoModel toModel(FormaPagamento formaPagamento) {
+      return modelMapper.map(formaPagamento, FormaPagamentoModel.class);
+    }
+
+    public List<FormaPagamentoModel> toCollectionModel(List<FormaPagamento> formaPagamentos){
+        return formaPagamentos.stream()
+                .map(formaPagamento -> toModel(formaPagamento))
+                .collect(Collectors.toList());
+    }
+}
