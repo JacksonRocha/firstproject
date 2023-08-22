fa364752936fb630b7d48f5f7fb678dccf72bdbf
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-22 18:12:54 -0300

    12.5 Criando a classe FormaPagamentoInputDisassembler.java

---
+++ src/main/java/io/github/jackson/api/assembler/FormaPagamentoInputDisassembler.java
@@ -0,0 +1,24 @@
+package io.github.jackson.api.assembler;
+
+import io.github.jackson.api.model.mixin.input.RestauranteInput;
+import io.github.jackson.domain.model.Cozinha;
+import io.github.jackson.domain.model.FormaPagamento;
+import io.github.jackson.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FormaPagamentoInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public FormaPagamento toDomainObject(FormaPagamentoInput formaPagamentoInput) {
+        return modelMapper.map(formaPagamentoInput, FormaPagamento.class);
+    }
+
+    public void copyToDomainObject(FormaPagamentoInput formaPagamentoInput, FormaPagamento formaPagamento) {
+        modelMapper.map(formaPagamentoInput, formaPagamento);
+    }
+}
