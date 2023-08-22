2bf9991fd5207da57338c9aaf61b5e30cfefc227
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-22 18:07:14 -0300

    12.5 Criando a classe FormaPagamentoModel.java

---
+++ src/main/java/io/github/jackson/api/assembler/FormaPagamentoModel.java
@@ -0,0 +1,12 @@
+package io.github.jackson.api.assembler;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class FormaPagamentoModel {
+
+    private Long id;
+    private String descricao;
+}
