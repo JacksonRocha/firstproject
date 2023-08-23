10712746a2574b39fd187f991eb9b6416c4ed949
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-23 15:57:38 -0300

    12.7 Criando uma referencia de entrada de CidadeIdInput.java

---
+++ src/main/java/io/github/jackson/api/model/mixin/input/EndercoInput.java
@@ -0,0 +1,13 @@
+package io.github.jackson.api.model.mixin.input;
+
+import io.github.jackson.api.model.CidadeResumoModel;
+
+public class EndercoInput {
+
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private CidadeIdInput cidade;
+}
