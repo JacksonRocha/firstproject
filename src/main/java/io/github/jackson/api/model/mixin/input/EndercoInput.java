d1fdfcb4d1554bb41c32d47dea0de8cdc4994c42
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-23 16:06:16 -0300

    12.7 Criando obrigatoriedade nos endereços dos Restaurantes em EndercoInput.java

--- src/main/java/io/github/jackson/api/model/mixin/input/EndercoInput.java
+++ src/main/java/io/github/jackson/api/model/mixin/input/EndercoInput.java
@@ -1,13 +1,30 @@
 package io.github.jackson.api.model.mixin.input;
 
-import io.github.jackson.api.model.CidadeResumoModel;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class EndercoInput {
 
+    @NotBlank
     private String cep;
+
+    @NotBlank
     private String logradouro;
+
+    @NotBlank
     private String numero;
+
     private String complemento;
+
+    @NotBlank
     private String bairro;
+
+    @Valid
+    @NotNull
     private CidadeIdInput cidade;
 }
