2e1a7415b3ca9a0c2af2b8332c993338e8f482b3
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-23 15:46:09 -0300

    12.6 Fazendo a cofiguração para que o tipo da propriedade do DTO seja mostrado corretamente na ModelMapperConfig.java

--- src/main/java/io/github/jackson/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/io/github/jackson/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package io.github.jackson.core.modelmapper;
 
+import io.github.jackson.api.model.EnderecoModel;
+import io.github.jackson.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,15 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+            var modelMapper = new ModelMapper();
+
+        var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
+                Endereco.class, EnderecoModel.class);
+
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+                enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+                (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
+
+        return modelMapper;
     }
 }
