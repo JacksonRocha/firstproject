c0074fab12fccbf658c8777ffd922a7cbc57f686
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-24 20:49:33 -0300

    12.8 Criando a classe de serviço CadastroGrupoService.java criando o metodo excluir

--- src/main/java/io/github/jackson/domain/service/CadastroGrupoService.java
+++ src/main/java/io/github/jackson/domain/service/CadastroGrupoService.java
@@ -1,20 +1,40 @@
 package io.github.jackson.domain.service;
 
+import io.github.jackson.domain.exception.EntidadeEmUsoException;
+import io.github.jackson.domain.exception.GrupoNaoEncontradaException;
 import io.github.jackson.domain.model.Grupo;
 import io.github.jackson.domain.repository.GrupoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CadastroGrupoService {
 
+    public static final String MSG_GRUPO_EM_USO = "Grupo de código %d não poder ser removido, pois está em uso";
     @Autowired
     private GrupoRepository grupoRepository;
 
     @Transactional
     public Grupo salvar(Grupo grupo) {
         return grupoRepository.save(grupo);
+    }
+
+    @Transactional
+    public void excluir(Long grupoId) {
+        try {
+            grupoRepository.deleteById(grupoId);
+            grupoRepository.flush();
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new GrupoNaoEncontradaException(grupoId);
+
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format(MSG_GRUPO_EM_USO, grupoId));
+        }
     }
 
 
