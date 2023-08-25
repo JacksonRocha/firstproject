9ec85d2bea1f1f6dfb2586e665cd01945c566c84
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-08-24 21:19:36 -0300

    12.8 Criando a controller de Grupos em GrupoController.java criando o metodo buscar

--- src/main/java/io/github/jackson/api/controller/GrupoController.java
+++ src/main/java/io/github/jackson/api/controller/GrupoController.java
@@ -2,9 +2,13 @@ package io.github.jackson.api.controller;
 
 import io.github.jackson.api.assembler.GrupoInputDisassembler;
 import io.github.jackson.api.assembler.GrupoModelAssembler;
+import io.github.jackson.api.model.GrupoModel;
+import io.github.jackson.domain.model.Grupo;
 import io.github.jackson.domain.repository.GrupoRepository;
 import io.github.jackson.domain.service.CadastroGrupoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +27,14 @@ public class GrupoController {
 
     @Autowired
     private GrupoInputDisassembler grupoInputDisassembler;
+
+    @GetMapping("/{grupoId}")
+    public GrupoModel buscar(@PathVariable Long grupoId) {
+        Grupo grupo = cadastroGrupoService.buscarOuFalhar(grupoId);
+
+        return grupoModelAssembler.toModel(grupo);
+    }
+
 
 
 }
