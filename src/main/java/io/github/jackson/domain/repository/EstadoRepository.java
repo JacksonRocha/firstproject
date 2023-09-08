cd11cf19bf9edeafa6eda5c662da79a88d739d0b
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:53:38 -0300

    12.11 Extendendo a CustomJpaRepository

--- src/main/java/io/github/jackson/domain/repository/EstadoRepository.java
+++ src/main/java/io/github/jackson/domain/repository/EstadoRepository.java
@@ -1,10 +1,11 @@
 package io.github.jackson.domain.repository;
 
 import io.github.jackson.domain.model.Estado;
+import io.github.jackson.domain.model.Usuario;
 import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface EstadoRepository extends JpaRepository<Estado, Long> {
+public interface EstadoRepository extends JpaRepository<Estado, Long>, CustomJpaRepository<Estado, Long> {
 }
