b862ffa87262a8deb64516bce54a33847c76a7ec
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:55:41 -0300

    12.11 Acrescetando a anotação

--- src/main/java/io/github/jackson/domain/repository/PermissaoRepository.java
+++ src/main/java/io/github/jackson/domain/repository/PermissaoRepository.java
@@ -3,8 +3,9 @@ package io.github.jackson.domain.repository;
 import io.github.jackson.domain.model.Permissao;
 import io.github.jackson.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface PermissaoRepository extends JpaRepository<Permissao, Long>, CustomJpaRepository<Permissao, Long> {
 }
