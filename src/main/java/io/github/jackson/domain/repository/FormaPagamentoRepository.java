dc8aae447719b1fec9684f9e6fceb26c46a8bab7
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:53:46 -0300

    12.11 Extendendo a CustomJpaRepository

--- src/main/java/io/github/jackson/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/io/github/jackson/domain/repository/FormaPagamentoRepository.java
@@ -1,9 +1,10 @@
 package io.github.jackson.domain.repository;
 
 import io.github.jackson.domain.model.FormaPagamento;
+import io.github.jackson.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long>, CustomJpaRepository<FormaPagamento, Long> {
 }
