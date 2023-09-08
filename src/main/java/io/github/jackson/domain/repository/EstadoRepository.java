ae292f6565adfeff826f84d4ad9f4129db2d6158
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:56:15 -0300

    12.11 Acrescetando a anotação

--- src/main/java/io/github/jackson/domain/repository/EstadoRepository.java
+++ src/main/java/io/github/jackson/domain/repository/EstadoRepository.java
@@ -4,8 +4,9 @@ import io.github.jackson.domain.model.Estado;
 import io.github.jackson.domain.model.Usuario;
 import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface EstadoRepository extends JpaRepository<Estado, Long>, CustomJpaRepository<Estado, Long> {
 }
