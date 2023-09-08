d9396aa30fef21b36aae45d6f82f8e5ac07f4185
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:56:00 -0300

    12.11 Acrescetando a anotação

--- src/main/java/io/github/jackson/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/io/github/jackson/domain/repository/FormaPagamentoRepository.java
@@ -3,8 +3,9 @@ package io.github.jackson.domain.repository;
 import io.github.jackson.domain.model.FormaPagamento;
 import io.github.jackson.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long>, CustomJpaRepository<FormaPagamento, Long> {
 }
