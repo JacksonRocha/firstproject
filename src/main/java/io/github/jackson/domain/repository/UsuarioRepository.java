5a72ea37eb5649f5087538b0de8c91bd08f42121
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:51:43 -0300

    12.11 Extendendo a CustomJpaRepository

--- src/main/java/io/github/jackson/domain/repository/UsuarioRepository.java
+++ src/main/java/io/github/jackson/domain/repository/UsuarioRepository.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>, CustomJpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
 
