02149551fb25ceb2599555d4464ff418661052e8
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:53:54 -0300

    12.11 Extendendo a CustomJpaRepository

--- src/main/java/io/github/jackson/domain/repository/GrupoRepository.java
+++ src/main/java/io/github/jackson/domain/repository/GrupoRepository.java
@@ -1,10 +1,11 @@
 package io.github.jackson.domain.repository;
 
 import io.github.jackson.domain.model.Grupo;
+import io.github.jackson.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GrupoRepository
-        extends JpaRepository<Grupo,Long> {
+        extends JpaRepository<Grupo,Long>, CustomJpaRepository<Grupo, Long> {
 }
