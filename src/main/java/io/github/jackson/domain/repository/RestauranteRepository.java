bf53b0c62ed1c95d7ad8f54b5fc5807484441303
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:55:18 -0300

    12.11 Acrescetando a anotação

--- src/main/java/io/github/jackson/domain/repository/RestauranteRepository.java
+++ src/main/java/io/github/jackson/domain/repository/RestauranteRepository.java
@@ -6,12 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface RestauranteRepository
         extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
         JpaSpecificationExecutor<Restaurante> {
