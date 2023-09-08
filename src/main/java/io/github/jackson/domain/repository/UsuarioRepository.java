5ec00e65a92a8143cca78f9c068c21fa6e29a9fa
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 19:57:35 -0300

    12.11 Criando o metodo para validar email do usuario

--- src/main/java/io/github/jackson/domain/repository/UsuarioRepository.java
+++ src/main/java/io/github/jackson/domain/repository/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package io.github.jackson.domain.repository;
 
-import io.github.jackson.domain.model.Grupo;
 import io.github.jackson.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
 }
