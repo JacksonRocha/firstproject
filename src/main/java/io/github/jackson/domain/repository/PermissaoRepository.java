7700aba9d19c5afba7d5210822fec8c1c744e048
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:54:06 -0300

    12.11 Extendendo a CustomJpaRepository

--- src/main/java/io/github/jackson/domain/repository/PermissaoRepository.java
+++ src/main/java/io/github/jackson/domain/repository/PermissaoRepository.java
@@ -1,9 +1,10 @@
 package io.github.jackson.domain.repository;
 
 import io.github.jackson.domain.model.Permissao;
+import io.github.jackson.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
+public interface PermissaoRepository extends JpaRepository<Permissao, Long>, CustomJpaRepository<Permissao, Long> {
 }
