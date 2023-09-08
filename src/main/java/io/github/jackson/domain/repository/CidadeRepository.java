fa1490e0c7237fb164bbbbd9f0d09584bdf0e810
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 20:53:20 -0300

    12.11 Extendendo a CustomJpaRepository

--- src/main/java/io/github/jackson/domain/repository/CidadeRepository.java
+++ src/main/java/io/github/jackson/domain/repository/CidadeRepository.java
@@ -2,10 +2,11 @@ package io.github.jackson.domain.repository;
 
 
 import io.github.jackson.domain.model.Cidade;
+import io.github.jackson.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+public interface CidadeRepository extends JpaRepository<Cidade, Long>, CustomJpaRepository<Cidade, Long> {
 }
