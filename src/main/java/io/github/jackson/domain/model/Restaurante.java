f98bda8cfb4d78a7ef606ad63d7de973c499e9ac
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 21:51:27 -0300

    12.12 Mudando a LISTA para uma SET para não duplicar dados

--- src/main/java/io/github/jackson/domain/model/Restaurante.java
+++ src/main/java/io/github/jackson/domain/model/Restaurante.java
@@ -10,7 +10,9 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @ValorZeroIncluiDescricao(valorField = "taxaFrete",
 descricaoField = "nome", descricaoObrigatorio = "Frete Grátis")
@@ -51,7 +53,7 @@ public class Restaurante {
     @JoinTable(name = "restaurante_forma_pagamento",
     joinColumns = @JoinColumn(name = "restaurante_id"),
     inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-    private List<FormaPagamento> formasPagamento = new ArrayList<>();
+    private Set<FormaPagamento> formasPagamento = new HashSet<>();
 
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produtos = new ArrayList<>();
