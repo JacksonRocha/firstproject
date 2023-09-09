601944ef611b0119116eb6c8eee7a287b86dca40
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-08 21:38:29 -0300

    12.12 Criando o endpoint que disvincula uma forma de pagamento

--- src/main/java/io/github/jackson/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/io/github/jackson/api/controller/RestauranteFormaPagamentoController.java
@@ -1,37 +1,16 @@
 package io.github.jackson.api.controller;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.jackson.api.assembler.FormaPagamentoInputDisassembler;
 import io.github.jackson.api.assembler.FormaPagamentoModelAssembler;
-import io.github.jackson.api.assembler.RestauranteInputDisassembler;
-import io.github.jackson.api.assembler.RestauranteModelAssembler;
 import io.github.jackson.api.model.FormaPagamentoModel;
-import io.github.jackson.api.model.RestauranteModel;
-import io.github.jackson.api.model.mixin.input.RestauranteInput;
-import io.github.jackson.domain.exception.CidadeNaoEncontradaException;
-import io.github.jackson.domain.exception.CozinhaNaoEncontradaException;
-import io.github.jackson.domain.exception.NegocioException;
-import io.github.jackson.domain.exception.RestauranteNaoEncontradaException;
-import io.github.jackson.domain.model.FormaPagamento;
 import io.github.jackson.domain.model.Restaurante;
-import io.github.jackson.domain.repository.RestauranteRepository;
 import io.github.jackson.domain.service.CadastroRestauranteService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.validation.SmartValidator;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/restaurantes/{restauranteId}/formas-pagamento", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -52,5 +31,10 @@ public class RestauranteFormaPagamentoController {
         Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
 
         return formaPagamentoModelAssembler.toCollectionModel(restaurante.getFormasPagamento());
+    }
+    @DeleteMapping("/{formaPagamentoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void desassociar(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
+        cadastroRestauranteService.desassociarFormaPagamento(restauranteId, formaPagamentoId);
     }
  }
