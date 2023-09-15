c6eab8cb5c27729b78ab2fc39ecff668b3a0e66c
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-14 21:18:19 -0300

    12.13 Alterando a service

--- src/main/java/io/github/jackson/domain/service/CadastroProdutoService.java
+++ src/main/java/io/github/jackson/domain/service/CadastroProdutoService.java
@@ -1,20 +1,14 @@
 package io.github.jackson.domain.service;
 
-import io.github.jackson.domain.exception.EntidadeEmUsoException;
-import io.github.jackson.domain.exception.GrupoNaoEncontradaException;
 import io.github.jackson.domain.exception.ProdutoNaoEncontradoException;
-import io.github.jackson.domain.model.Grupo;
 import io.github.jackson.domain.model.Produto;
-import io.github.jackson.domain.repository.GrupoRepository;
 import io.github.jackson.domain.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class ProdutoGrupoService {
+public class CadastroProdutoService {
 
     @Autowired
     private ProdutoRepository produtoRepository;
