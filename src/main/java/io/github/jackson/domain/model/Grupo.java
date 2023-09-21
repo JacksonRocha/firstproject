4b120a026acbbc6448b1319b65728311885d4b4b
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-21 20:23:14 -0300

    12.15 Alterando o tipo da propriedade permissao e criando dois metodos para adicionar e remover a permissao

--- src/main/java/io/github/jackson/domain/model/Grupo.java
+++ src/main/java/io/github/jackson/domain/model/Grupo.java
@@ -6,7 +6,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -26,7 +28,15 @@ public class Grupo {
     @JoinTable(name = "grupo_permissao",
             joinColumns = @JoinColumn(name = "grupo_id"),
             inverseJoinColumns = @JoinColumn(name = "permissao_id"))
-    private List<Permissao> permissoes = new ArrayList<>();
+    private Set<Permissao> permissoes = new HashSet<>();
+
+    public boolean removerPermissao(Permissao permissao) {
+        return getPermissoes().remove(permissao);
+    }
+
+    public boolean adicionarPermissao(Permissao permissao) {
+        return getPermissoes().add(permissao);
+    }
 
 
 }
