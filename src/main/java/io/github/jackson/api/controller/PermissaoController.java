a6a0291be6d22def181548fa1b38199e93e0028d
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-21 20:40:36 -0300

    12.15 Criando os endpoint de Permissao

---
+++ src/main/java/io/github/jackson/api/controller/PermissaoController.java
@@ -0,0 +1,41 @@
+package io.github.jackson.api.controller;
+
+import io.github.jackson.api.assembler.PermissaoModelAssembler;
+import io.github.jackson.api.model.PermissaoModel;
+import io.github.jackson.domain.model.Grupo;
+import io.github.jackson.domain.service.CadastroGrupoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/grupos/{grupoId}/permissoes")
+public class PermissaoController {
+
+    @Autowired
+    private CadastroGrupoService cadastroGrupoService;
+
+    @Autowired
+    private PermissaoModelAssembler permissaoModelAssembler;
+
+    @GetMapping
+    public List<PermissaoModel> listar(@PathVariable Long grupoId) {
+        Grupo grupo = cadastroGrupoService.buscarOuFalhar(grupoId);
+
+        return permissaoModelAssembler.toCollectionModel(grupo.getPermissoes());
+    }
+
+    @DeleteMapping("/{permissaoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void desassociar(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
+        cadastroGrupoService.desassociarPermissao(grupoId, permissaoId);
+    }
+
+    @PutMapping("/{permissaoId}")
+    public void associar (@PathVariable Long grupoId, @PathVariable Long permissaoId) {
+        cadastroGrupoService.associarPermissao(grupoId, permissaoId);
+    }
+
+}
