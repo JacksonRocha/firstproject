aa3334a39b7e9182b3702f54d75a15fc115851c5
Jackson_Rocha <50529134+JacksonRocha@users.noreply.github.com>
2023-09-21 21:03:01 -0300

    12.16 Alterando o tipo da propriedade usuario e criando metodos para adicionar e remover a entidade

--- src/main/java/io/github/jackson/domain/model/Usuario.java
+++ src/main/java/io/github/jackson/domain/model/Usuario.java
@@ -12,7 +12,9 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -41,7 +43,16 @@ public class Usuario {
     @JoinTable(name = "usuario_grupo",
             joinColumns = @JoinColumn(name = "usuario_id"),
             inverseJoinColumns = @JoinColumn(name = "grupo_id"))
-    private List<Grupo> grupos = new ArrayList<>();
+    private Set<Grupo> grupos = new HashSet<>();
+
+    public boolean removerGrupo(Grupo grupo) {
+        return getGrupos().remove(grupo);
+    }
+
+    public boolean adicionarGrupo(Grupo grupo) {
+        return getGrupos().add(grupo);
+    }
+
 
     public boolean senhaCoincideCom(String senha) {
         return getSenha().equals(senha);
